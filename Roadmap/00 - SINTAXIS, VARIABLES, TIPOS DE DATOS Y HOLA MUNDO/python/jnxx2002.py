456196d9dc74660f5324003e7abdd2a68ac3f238
jnxx2002 <pereiracorderojuandiego@gmail.com>
2024-12-11 09:42:36 -0400

    jnxx2002.py
    mi primera vez aqui jeje

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/jnxx2002.py
@@ -0,0 +1,17 @@
+# hola este es el url del lenguaje, es mi primera vez programando https://www.python.org/
+
+# Comentario en una linea
+
+variable = "primera variable"
+
+# python como tal no tiene constante pero por ejemplo puedo colocar una variable
+# Ej. CONSTANTE = "una constante"   pero esta no debe variar, ademas de estar en mayusculas
+
+una_variable = "esto es una cadena de texto"  # o string
+otra_variable = 'no se que escribir'
+int = 15  # esto es un entero o int
+float = 14.6  # esto es un flotante o float
+bool = True
+bool = False
+
+print("Hola python")
